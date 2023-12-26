ce8c9b6b5f9c33779c4e23c7f8f93279ad27a73e
hyunjialee <hyjialee@gmail.com>
2023-12-26 16:06:59 -0500

    finsihed

--- src/main/java/com/zipcodewilmington/bakery/BakeryApplication.java
+++ src/main/java/com/zipcodewilmington/bakery/BakeryApplication.java
@@ -2,6 +2,10 @@ package com.zipcodewilmington.bakery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.annotation.WebServlet;
 
 @SpringBootApplication
 public class BakeryApplication {
@@ -9,4 +13,5 @@ public class BakeryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BakeryApplication.class, args);
 	}
+
 }
--- src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
@@ -1,33 +1,44 @@
 package com.zipcodewilmington.bakery.controllers;
-
 import com.zipcodewilmington.bakery.models.Muffin;
 import com.zipcodewilmington.bakery.services.MuffinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class MuffinController {
     private MuffinService service;
 
+    @Autowired
     public MuffinController(MuffinService service) {
         this.service = service;
     }
 
+    @RequestMapping(value = "/muffins/all", method = RequestMethod.GET)
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> show(Long id) {
+    @RequestMapping(value = "/muffins/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
-
-    public ResponseEntity<Muffin> create(Muffin baker) {
+    @RequestMapping(value = "/muffins", method = RequestMethod.POST)
+    public ResponseEntity<Muffin> create(@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
+    @RequestMapping(value = "muffins/update/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Muffin> update(Long id, Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/muffins/destroy/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Boolean> destroy(Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
